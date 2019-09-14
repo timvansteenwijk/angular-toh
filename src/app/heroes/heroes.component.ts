9e4bc46880b909bfbce05aff3c2164c2bb0b9c3c
Steenwijk <tim-van.steenwijk@klm.com>
2019-09-14 15:04:52 +0200

    split hero/detail components

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -12,9 +13,14 @@ export class HeroesComponent implements OnInit {
     name: 'Windstorm'
   };
 
-  constructor() { }
+  heroes = HEROES;
 
-  ngOnInit() {
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
+  constructor() {}
+
+  ngOnInit() {}
 }
